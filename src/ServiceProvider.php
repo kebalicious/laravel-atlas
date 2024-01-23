b25263a486198db3dbe2a23a55baef29572444e3
Kebal <tsmiqbalhakim@gmail.com>
2024-01-23 14:38:17 +0800

    update

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,16 +2,14 @@
 
 namespace LaravelAtlas;
 
-use Route;
 use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
 
-class ServiceProvider extends IlluminateServiceProvider
+class AtlasServiceProvider extends IlluminateServiceProvider
 {
-
     /**
      * Register.
      *
-     * @return
+     * @return void
      */
     public function register()
     {
@@ -43,5 +41,43 @@ class ServiceProvider extends IlluminateServiceProvider
         Route::get(config('laravel-atlas.url'), 'LaravelAtlas\LaravelAtlasController@show')
             ->name('laravel-atlas.show')
             ->middleware(config('laravel-atlas.middlewares'));
+
+        // Add the following code to copy assets during the post-autoload-dump event
+        $this->app->booted(function () {
+            // Define the source and destination paths
+            $sourcePath = __DIR__ . '/public';
+            $destinationPath = public_path();
+
+            // Copy the files using a recursive directory copy function
+            $this->recursiveCopy($sourcePath, $destinationPath);
+
+            // Optional: Display a message to indicate successful copying
+            echo "LaravelAtlas assets have been successfully copied to the Laravel public folder.\n";
+        });
+    }
+
+    // Recursive directory copy function
+    protected function recursiveCopy($source, $destination)
+    {
+        $directory = opendir($source);
+
+        if (!is_dir($destination)) {
+            mkdir($destination, 0755, true);
+        }
+
+        while ($file = readdir($directory)) {
+            if ($file !== "." && $file !== "..") {
+                $sourceFile = $source . '/' . $file;
+                $destinationFile = $destination . '/' . $file;
+
+                if (is_dir($sourceFile)) {
+                    $this->recursiveCopy($sourceFile, $destinationFile);
+                } else {
+                    copy($sourceFile, $destinationFile);
+                }
+            }
+        }
+
+        closedir($directory);
     }
 }
